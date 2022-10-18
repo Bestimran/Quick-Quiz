8a57e5104f15226489ddb1586b8d00eeaef987b4
Bestimran <imrantahir9918@gmail.com>
2022-10-18 13:38:10 -0700

    Header section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import img from "../../image/Screenshot_1.png";
-
+import './Home.css';
 const Home = () => {
   return (
     <div className=" home-page container mt-5 d-flex ">
- 
+      <div className="home-section">
+      <h1>It's time to Learn Web Developing!!</h1>
+      <h4>if would you like to enjoy your life start web developing!!!</h4>
+      <button>Learn more</button>
+      </div>
+      <img src={img} alt="" />
+     
     </div>
   );
 };
